46fd4c7cfe29b30036f06c00b4f62d87ddf858ef
Max Voloshchuk <68013193+Zarathustra4@users.noreply.github.com>
2025-02-02 17:04:52 +0200

    word detector returns word images in correct order (#64)

--- src/detection/bounding_boxes/word_to_lines.py
+++ src/detection/bounding_boxes/word_to_lines.py
@@ -1,5 +1,4 @@
 from skimage.draw import polygon2mask
-import matplotlib.pyplot as plt
 from typing import Optional
 from enum import Enum, auto
 import numpy as np
@@ -9,7 +8,7 @@ import cv2
 import os
 
 from .shapes import Bbox, Obb
-from .lines import line_angle
+from .obb_helper import line_angle
 from . import shapes as sh
 
 
@@ -55,12 +54,12 @@ def mean_image_color(image: np.ndarray) -> np.ndarray:
 
 
 def crop_line_from_image(
-        image: np.ndarray,
-        line: Obb,
-        rotate: bool = True,
-        mask_image: bool = True,
-        mask_color: CropMaskColor = CropMaskColor.BLACK
-    ) -> np.ndarray:
+    image: np.ndarray,
+    line: Obb,
+    rotate: bool = True,
+    mask_image: bool = True,
+    mask_color: CropMaskColor = CropMaskColor.BLACK,
+) -> np.ndarray:
     line = sh.obb_to_image_coords(image.shape[1], image.shape[0], line)
     np_line = obb_to_nparray(line)
     for i in range(np_line.shape[0]):
@@ -70,26 +69,24 @@ def crop_line_from_image(
 
     line_mask = np.stack([line_mask, line_mask, line_mask], axis=-1)
     x, y, w, h = obb_to_rec(line)
-    
-    cropped = image[y:y+h, x:x+w].copy()
+
+    cropped = image[y : y + h, x : x + w].copy()
     if any(dim == 0 for dim in cropped.shape):
         return cropped
 
     if mask_image:
-        cropped_mask = line_mask[y:y+h, x:x+w].copy()
+        cropped_mask = line_mask[y : y + h, x : x + w].copy()
         cropped = cropped * cropped_mask
 
     if rotate:
         rotation_matrix = cv2.getRotationMatrix2D(
-            (cropped.shape[1] // 2, cropped.shape[0] // 2), 
-            line_angle(line), 
-            scale=1
+            (cropped.shape[1] // 2, cropped.shape[0] // 2), line_angle(line), scale=1
         )
         cropped = cv2.warpAffine(
-            cropped, 
-            rotation_matrix, 
+            cropped,
+            rotation_matrix,
             (cropped.shape[1], cropped.shape[0]),
-            borderMode=cv2.BORDER_CONSTANT
+            borderMode=cv2.BORDER_CONSTANT,
         )
 
     if mask_color == CropMaskColor.WHITE:
@@ -106,17 +103,15 @@ def bbox_centers_to_numpy(words: list[Bbox]) -> np.ndarray:
     centers = []
     for word in words:
         centers.append([word.cx, word.cy])
-    
+
     return np.array(centers)
 
 
 def rotate_words_in_line(
-        rotation_angle: float,
-        img_shape: tuple[int, int],
-        words: list[Bbox]
-    ) -> list[Bbox]:
+    rotation_angle: float, img_shape: tuple[int, int], words: list[Bbox]
+) -> list[Bbox]:
     """
-    Rotate words' centers around to compensate line rotation. 
+    Rotate words' centers around to compensate line rotation.
     Args:
         rotation_angle (float): Line angle in degrees.
         img_shape (tuple[int, int]): Shape of cropped line image. Used to define center and make rotation accurate.
@@ -126,16 +121,18 @@ def rotate_words_in_line(
     """
     img_shape = np.array(img_shape)
     rotation_angle = np.radians(rotation_angle)
-    rotation_matrix = np.array([
-        [np.cos(rotation_angle), -np.sin(rotation_angle)],
-        [np.sin(rotation_angle), np.cos(rotation_angle)]
-    ])
+    rotation_matrix = np.array(
+        [
+            [np.cos(rotation_angle), -np.sin(rotation_angle)],
+            [np.sin(rotation_angle), np.cos(rotation_angle)],
+        ]
+    )
 
     rotation_center = img_shape / 2
 
     centers = bbox_centers_to_numpy(words) * img_shape
     centers -= rotation_center
-    rotated_centers = centers @ rotation_matrix.T # (A @ B).T = B.T @ A.T
+    rotated_centers = centers @ rotation_matrix.T  # (A @ B).T = B.T @ A.T
     rotated_centers += rotation_center
     rotated_centers /= img_shape
 
@@ -146,7 +143,7 @@ def rotate_words_in_line(
         rotated_bbox = sh.Bbox(center[0], center[1], bbox.w, bbox.h)
         cropped_bbox = crop_bbox(rotated_bbox)
         rotated_words.append(cropped_bbox)
-    
+
     return rotated_words
 
 
@@ -162,7 +159,7 @@ def crop_bbox(bbox: Bbox) -> Bbox:
             rec[i] = 1
         elif rec[i] < 0:
             rec[i] = 0
-    
+
     cx = (rec[0] + rec[2]) / 2
     cy = (rec[1] + rec[3]) / 2
     w = rec[2] - rec[0]
@@ -191,9 +188,7 @@ def bbox_to_line_space(bbox: Bbox, line: list, crop=True) -> Bbox:
     line_bbox_size = bbox_size / line_size
 
     if crop:
-        return crop_bbox(
-            Bbox(*line_bbox_center, *line_bbox_size)
-        ) 
+        return crop_bbox(Bbox(*line_bbox_center, *line_bbox_size))
 
     return Bbox(*line_bbox_center, *line_bbox_size)
 
@@ -209,30 +204,32 @@ def words_to_line_space(words: list[Bbox], line: Obb) -> list[Bbox]:
 
 
 def write_bboxes_to_file(bboxes: list[Bbox], filename: str, class_name: str) -> None:
-    with open(filename, 'w') as file:
+    with open(filename, "w") as file:
         for bbox in bboxes:
             line = class_name + " " + " ".join(map(str, bbox)) + "\n"
             file.write(line)
 
 
-def create_lines_dataset_from_image(image_path: str,
-                                    lines: list[Obb],
-                                    words: list[Bbox],
-                                    output_dir: str,
-                                    rotate: bool,
-                                    mask_image: bool,
-                                    mask_color: CropMaskColor,
-                                    word_class: str = '0') -> None:
+def create_lines_dataset_from_image(
+    image_path: str,
+    lines: list[Obb],
+    words: list[Bbox],
+    output_dir: str,
+    rotate: bool,
+    mask_image: bool,
+    mask_color: CropMaskColor,
+    word_class: str = "0",
+) -> None:
     if not os.path.exists(image_path):
         raise ValueError(f"There is no such an image {image_path}")
-    
+
     image_name = os.path.basename(image_path)
 
     image = cv2.imread(image_path)
 
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
-    
+
     line_to_words = sh.map_words_to_lines(words, lines, image)
     for line_idx in line_to_words:
         line = lines[line_idx]
@@ -244,18 +241,20 @@ def create_lines_dataset_from_image(image_path: str,
             cv2.imwrite(line_image_path, line_image)
         except:
             print(f"\t[WARNING] Failed to save line image with index {line_idx}")
-        
+
         line_words = [words[idx] for idx in line_to_words[line_idx]]
         line_words = words_to_line_space(line_words, line)
         if rotate and len(line_words):
             rotation_angle = -line_angle(line)
             line_words = rotate_words_in_line(
-                rotation_angle, 
-                (line_image.shape[1], line_image.shape[0]),
-                line_words
+                rotation_angle, (line_image.shape[1], line_image.shape[0]), line_words
             )
 
-        write_bboxes_to_file(line_words, os.path.join(output_dir, f"{image_name[:-4]}_{line_idx}.txt"), word_class)
+        write_bboxes_to_file(
+            line_words,
+            os.path.join(output_dir, f"{image_name[:-4]}_{line_idx}.txt"),
+            word_class,
+        )
 
 
 def find_line_label(word_label_path: str, line_labels: list[str]) -> Optional[str]:
@@ -265,25 +264,31 @@ def find_line_label(word_label_path: str, line_labels: list[str]) -> Optional[st
             return line_label
 
 
-def create_dataset_of_lines(words_dataset_path: str,
-                            words_ids: list,
-                            lines_dataset_path: str,
-                            lines_ids: list,
-                            output_dataset_path: str,
-                            rotate: bool,
-                            mask_image: bool,
-                            mask_color: CropMaskColor):
+def create_dataset_of_lines(
+    words_dataset_path: str,
+    words_ids: list,
+    lines_dataset_path: str,
+    lines_ids: list,
+    output_dataset_path: str,
+    rotate: bool,
+    mask_image: bool,
+    mask_color: CropMaskColor,
+):
     os.makedirs(output_dataset_path, exist_ok=True)
     word_label_paths: list[str] = []
     line_label_paths: list[str] = []
 
-    word_subdirs = [subdir for subdir in os.scandir(words_dataset_path) if subdir.is_dir()]
+    word_subdirs = [
+        subdir for subdir in os.scandir(words_dataset_path) if subdir.is_dir()
+    ]
     for word_subdir in word_subdirs:
         for file in os.listdir(word_subdir.path):
             if file.endswith(".txt"):
                 word_label_paths.append(os.path.join(word_subdir.path, file))
 
-    line_subdirs = [subdir for subdir in os.scandir(lines_dataset_path) if subdir.is_dir()]
+    line_subdirs = [
+        subdir for subdir in os.scandir(lines_dataset_path) if subdir.is_dir()
+    ]
     for line_subdir in line_subdirs:
         for file in os.listdir(line_subdir.path):
             if file.endswith(".txt"):
@@ -294,34 +299,53 @@ def create_dataset_of_lines(words_dataset_path: str,
         word_bboxes = sh.read_shapes(word_label, sh.to_bbox, words_ids)
         line_label = find_line_label(word_label, line_label_paths)
         if line_label is None:
-            print(f"[WARNING] - {os.path.basename(word_label)} was not found in line labels")
+            print(
+                f"[WARNING] - {os.path.basename(word_label)} was not found in line labels"
+            )
             continue
-        
+
         print(f"[INFO] processing {os.path.basename(word_label).split('.')[0]}")
 
         line_obbs = sh.read_shapes(line_label, sh.to_obb, lines_ids)
 
         create_lines_dataset_from_image(
-            image_path, line_obbs, word_bboxes, 
-            output_dataset_path, rotate, mask_image, mask_color
+            image_path,
+            line_obbs,
+            word_bboxes,
+            output_dataset_path,
+            rotate,
+            mask_image,
+            mask_color,
         )
 
 
 def split_dataset(dataset_path: str, output_path: str):
     if not os.path.exists(output_path):
         os.mkdir(output_path)
-    
+
     if not os.path.exists(os.path.join(output_path, "train")):
         os.mkdir(os.path.join(output_path, "train"))
-    
+
     if not os.path.exists(os.path.join(output_path, "validation")):
         os.mkdir(os.path.join(output_path, "validation"))
-    
+
     for file in os.listdir(dataset_path):
         if file.endswith(".jpg"):
             if random.random() < 0.8:
-                shutil.move(os.path.join(dataset_path, file), os.path.join(output_path, "train", file))
-                shutil.move(os.path.join(dataset_path, file[:-4] + ".txt"), os.path.join(output_path, "train", file[:-4] + ".txt"))
+                shutil.move(
+                    os.path.join(dataset_path, file),
+                    os.path.join(output_path, "train", file),
+                )
+                shutil.move(
+                    os.path.join(dataset_path, file[:-4] + ".txt"),
+                    os.path.join(output_path, "train", file[:-4] + ".txt"),
+                )
             else:
-                shutil.move(os.path.join(dataset_path, file), os.path.join(output_path, "validation", file))
-                shutil.move(os.path.join(dataset_path, file[:-4] + ".txt"), os.path.join(output_path, "validation", file[:-4] + ".txt"))
+                shutil.move(
+                    os.path.join(dataset_path, file),
+                    os.path.join(output_path, "validation", file),
+                )
+                shutil.move(
+                    os.path.join(dataset_path, file[:-4] + ".txt"),
+                    os.path.join(output_path, "validation", file[:-4] + ".txt"),
+                )
